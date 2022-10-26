516a5c8dbeaefba47572f9b0709458d1526decf6
ocean2580 <2080917477@qq.com>
2022-10-26 13:30:07 +0800

    图标列表

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@
     <!-- 3.引用 -->
     <TopNav/>
     <SwiperTop/>
+    <IconList/>
   </div>
 </template>
 
@@ -12,12 +13,13 @@
 // 1.引入
 import TopNav from '@/components/home/TopNav.vue'
 import SwiperTop from '@/components/home/SwiperTop.vue'
+import IconList from '@/components/home/IconList.vue'
 
 export default {
   name: "HomeView",
   // 2.注册
   components: {
-    TopNav, SwiperTop
+    TopNav, SwiperTop, IconList
   },
 };
 </script>
