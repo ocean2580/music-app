287d29cc4db220ea236d40fb22369cccd4452f22
ocean2580 <2080917477@qq.com>
2022-10-28 20:59:49 +0800

    歌词与磁盘切换

--- src/components/item/MusicDetail.vue
+++ src/components/item/MusicDetail.vue
@@ -3,7 +3,7 @@
   <div class="detailTop">
     <div class="detailTopLeft">
       <svg t="1666770886096" class="icon back" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
-        p-id="23705" width="200" height="200" @click="updateDetailShow">
+        p-id="23705" width="200" height="200" @click="backHome">
         <!-- 点击返回首页 -->
         <path
           d="M862.485 481.154H234.126l203.3-203.3c12.497-12.497 12.497-32.758 0-45.255s-32.758-12.497-45.255 0L135.397 489.373c-12.497 12.497-12.497 32.758 0 45.254l256.774 256.775c6.249 6.248 14.438 9.372 22.627 9.372s16.379-3.124 22.627-9.372c12.497-12.497 12.497-32.759 0-45.255l-203.3-203.301h628.36c17.036 0 30.846-13.81 30.846-30.846s-13.81-30.846-30.846-30.846z"
@@ -37,15 +37,15 @@
     </div>
   </div>
 
-  <div class="detailContent" v-show="isLyricShow">
+  <div class="detailContent" v-show="!isLyricShow">
     <!-- 根据播放状态切换样式 -->
     <img src="@/assets/needle.png" alt="" class="img_needle" :class="{ img_needle_active: !isbtnShow }">
     <img src="@/assets/cd.png" alt="" class="img_cd">
     <img :src="musicList.al.picUrl" alt="" class="img_ar"
-      :class="{ img_ar_active: !isbtnShow, img_ar_paused: isbtnShow }">
+      :class="{ img_ar_active: !isbtnShow, img_ar_paused: isbtnShow }" @click="isLyricShow = true">
   </div>
 
-  <div class="musicLyric" ref="musicLyric">
+  <div class="musicLyric" ref="musicLyric" v-show="isLyricShow" @click="isLyricShow = false">
     <!-- 时间判断 -->
     <p v-for="item in lyric" :key="item"
       :class="{ active: (currentTime * 1000) >= item.time && (currentTime * 1000) < item.pre }">
@@ -174,6 +174,7 @@ import { mapMutations, mapState } from "vuex";
 export default {
   data() {
     return {
+      // 切换
       isLyricShow: false
     }
   },
@@ -221,6 +222,10 @@ export default {
     Vue3Marquee
   },
   methods: {
+    backHome: function () {
+      this.isLyricShow = false
+      this.updateDetailShow()
+    },
     ...mapMutations(['updateDetailShow'])
   }
 }
